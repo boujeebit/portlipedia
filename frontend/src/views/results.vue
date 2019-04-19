54018c98e059ecefa98f820a9b6a8643ad10487b
0xCODEs <0xCODEs@users.noreply.github.com>
2019-04-18 21:13:18 -0500

    Adding backend pagination

--- frontend/src/views/results.vue
+++ frontend/src/views/results.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
       <div class="results-search">
+        <div class="top-margin-10">
+          <div class="logo-container">
+            <img height="40" src="/static/img/logo.png" class="logo">
+          </div>
+        </div>
         <div class="search">
-          <input v-model="$route.params.query" v-on:keyup.enter="run" autofocus/>
-          <!-- <button class="search-button" v-on:click="run">Search</button> -->
+          <input v-model="search" v-on:keyup.enter="run" autofocus/>
+          <button class="search-button" v-on:click="run">Search</button>
         </div>
       </div>
-    results -- {{this.$route.params.query}}
 
 
 
     <br>
+    Loading: {{loading}} -- {{search}}
+
+
     <div class="code-block" v-if="output">
           <pre id="json">{{output}}</pre>
         </div>
@@ -25,15 +32,30 @@ export default {
   props: ['query'],
   data () {
     return {
-      output: ""
+      search: "",
+      output: "",
+      loading: false
     }
   },
   beforeMount (){
+    this.search = this.$route.params.query;
     let self = this;
-    api(`query { search(search:"${this.$route.params.query}") { name port protocol description } }`).then(data => {
+    api(`query { search(search:"${this.search}", skip:0, first:10) { name port protocol description } }`).then(data => {
       console.log(data.search)
       self.output = data.search;
     })
+  },
+  methods: {
+    run() {
+      this.output = "";
+      let self = this;
+      this.loading = true;
+      api(`query { search(search:"${this.search}", skip:0, first:10) { name port protocol description } }`).then(data => {
+        console.log(data.search)
+        self.output = data.search;
+        self.loading = false;
+      })
+    }
   }
 }
 </script>
